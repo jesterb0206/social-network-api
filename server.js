d6314dc5958b2dfee6beb3eafb62632ec2d2616e
jesterb0206 <jesterb@seattleu.edu>
2023-01-08 21:05:02 -0800

    Fixed server.js & Made Some Tiny Deletions to .gitignore & package.json

--- server.js
+++ server.js
@@ -1,36 +1,17 @@
-const dotenv = require('dotenv');
+/* eslint-disable no-console */
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('./config/connection');
 const routes = require('./routes');
 
-dotenv.config({ path: './config.env' });
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
-
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    // eslint-disable-next-line no-console
-    console.log('DB connection established!');
-  });
-
-const port = process.env.PORT || 3000;
-DB.once('open', () => {
-  app.listen(port, () => {
-    // eslint-disable-next-line no-console
-    console.log(`App listening on port ${port}...`);
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
   });
 });
